0e9f5145aff32cbed43628a3b9f791c0dcfa155d
Nhomar <nhomar@nhomar-laptop>
2009-10-20 14:38:18 -0430

    Initial import

---
+++ load_states_venezuela/__terp__.py
@@ -0,0 +1,20 @@
+# module_name/__terp__.py
+{
+  'name' : 'Load States Venezuela',
+  'version' : '0.1',
+  'author' : 'Tiny',
+  'website' : 'http://wiki.openerp.org.ve/index.php?title=Boceto_estados_municipios',
+  'category' : 'Tools',
+  'description' : 
+''''
+This Module only load Venezuela's States on object res.country.state
+Este Módulo solo carga los estados de venezuela en la base de datos llenando el objeto res.country.state
+''',
+  'depends' : ['base'],
+  'init_xml' : [],
+  'demo_xml' : [],
+  'update_xml' : ['load_states_venezuela_data.xml'],
+  'installable' : True,
+  'active' : False,
+}
+
