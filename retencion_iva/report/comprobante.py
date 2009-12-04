cb2c4b816def374f4a1bade35887c14a30d51073
Javier Duran <>
2009-12-03 19:57:30 -0430

    actualizacion del archivo comprobante.rml

--- retencion_iva/report/comprobante.py
+++ retencion_iva/report/comprobante.py
@@ -73,6 +73,55 @@ class rep_comprobante(report_sxw.rml_parse):
 
         return types[tipo]
 
+
+
+    def _get_totales(self, comp_id):        
+        if not comp_id:
+            return []
+        sql = """
+        SELECT    d.name,d.quantity_received,d.price_unit,d.discount,d.price_standard  
+        FROM    account_invoice_line AS d
+        WHERE    invoice_id=%d
+        ORDER    BY d.id;"""%order
+        self.cr.execute (sql)
+        resp = [] 
+
+        tot_comp = 0.0
+        tot_comp_sdc = 0.0
+        tot_base_imp = 0.0
+        tot_imp_iva = 0.0
+        tot_iva_ret = 0.0
+        comp_obj = self.pool.get('account.retention')
+        comp = comp_obj.browse(self.cr,self.uid, comp_id)
+
+        for rl in comp:
+
+        for inf in self.cr.fetchall():
+            total    = 0
+            totalcs    =0
+            desc    = 0 
+            if self.currentOrderId == 0 or self.currentOrderId != order:
+                self.currentOrderId = order
+                self.subtotal        = 0
+                self.totalgral        = 0
+                self.totalnd        = 0
+                self.subtotalcs        = 0
+                self.totalcxp        = 0
+            if (inf[1] > 0) and (inf[2] > 0):
+                total = inf[1]*inf[2]
+                totalcs = inf[1]*inf[4]
+                if inf[3] > 0:
+                    desc = total * inf[3] /100
+                    total -=  desc
+                self.subtotal    +=  total                            
+                self.subtotalcs    += totalcs
+                                
+            resp.append({"nomb":inf[0],"cant":inf[1],"preciop":inf[2],"precios":inf[4],"totalS":totalcs,"totalP":total})        
+        #Reserva 
+        self.totalreserva = self.subtotalcs - self.subtotal        
+        return resp        
+
+
     
       
 report_sxw.report_sxw(
