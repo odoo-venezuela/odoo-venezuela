8116570490dc99d4c827743e188f9087790eca41
Operador <operador@Satelite>
2009-12-27 09:49:47 -0430

    Corregido nombres según norma l10n_states_ve y agregado cmienzo de modelo para cities

---
+++ l10n_cities_ve/__terp__.py
@@ -0,0 +1,20 @@
+# module_name/__terp__.py
+{
+  'name' : 'States Venezuela',
+  'version' : '0.1',
+  'author' : 'Netquatro',
+  'website' : 'http://wiki.openerp.org.ve/index.php?title=Boceto_estados_municipios',
+  'category' : 'Localisation/Venezuela',
+  'description' : 
+'''
+This Module only load Venezuela's Cities on object res.country.city
+Este Módulo solo carga las Ciudades de venezuela en la base de datos llenando el objeto res.country.city
+''',
+  'depends' : ['base','l10n_states_ve'],
+  'init_xml' : [],
+  'demo_xml' : [],
+  'update_xml' : ['l10n_states_ve.xml'],
+  'installable' : True,
+  'active' : False,
+}
+
