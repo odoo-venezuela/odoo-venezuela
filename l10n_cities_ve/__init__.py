7c94f238b4683a77850f9e146b7a6545cca84944
Operador <operador@Satelite>
2009-12-31 16:38:44 -0430

    corregida vista cities

---
+++ l10n_cities_ve/__init__.py
@@ -0,0 +1,31 @@
+##############################################################################
+#
+#    OpenERP-Venezuela, Venezuelan Localization of OpenERP    
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://openerp.netquatro.com>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    OpenERP-Venezuela, Localizacion Venezolana de OpenERP    
+#    Copyright (C) 2010 Netquatro (<http://openerp.netquatro.com>). Todos los derechos Reservados
+#
+#    Este programa es un Software Libre lo puedes redistribuir y/o modificar
+#    Se encuentra bajo los terminos de licencia GNU
+#    publicada por "the Free Software Foundation,"
+#
+##############################################################################
+from osv import osv
+from osv import fields
+import country
+
