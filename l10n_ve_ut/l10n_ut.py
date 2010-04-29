d981462980eaa333dcd1cbbd32d527f0f8dc1e94
Javieredm@netquatro <>
2010-04-29 18:57:35 -0430

    [IMP] se agrega funciónes en el módelo l10n.ut para devolver la tasa de UT en una fecha dada y el calculo en UT de un monto dado, en el módulo l10n_ve_ut

--- l10n_ve_ut/l10n_ut.py
+++ l10n_ve_ut/l10n_ut.py
@@ -1,4 +1,32 @@
 # -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2009 Netquatro C.A. (http://openerp.netquatro.com/) All Rights Reserved.
+#                    Javier Duran <javier.duran@netquatro.com> 
+# 
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
 from osv import osv
 from osv import fields
 from tools import config
@@ -21,4 +49,24 @@ class l10n_ut(osv.osv):
     _defaults = {
         'name': lambda *a: None,
     }
+
+
+    def get_amount_ut(self, cr, uid, date=False, *args):
+        rate = 0
+        date= date or time.strftime('%Y-%m-%d')        
+        cr.execute("SELECT amount FROM l10n_ut WHERE date <= '%s' ORDER BY date desc LIMIT 1" % (date))
+        if cr.rowcount:
+            rate=cr.fetchall()[0][0]
+        return rate
+
+    def compute(self, cr, uid, from_amount, date=False, context={}):
+        result = 0.0
+        ut = self.get_amount_ut(cr, uid, date=False)
+        if ut:
+            result = from_amount / ut
+
+        return result
+
+
+
 l10n_ut()
