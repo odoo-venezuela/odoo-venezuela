a67cbf021befb59d61c5caea4ec1a4529f9e9381
javier@vauxoo.com <>
2011-06-14 12:36:20 -0430

    [FIX] corrección en el método onchange_partner_id de la factura, en el módulo l10n_ve_withholding_iva

--- l10n_ve_withholding_iva/invoice.py
+++ l10n_ve_withholding_iva/invoice.py
@@ -77,10 +77,10 @@ class account_invoice(osv.osv):
 
 
     def onchange_partner_id(self, cr, uid, ids, type, partner_id,
-            date_invoice=False, payment_term=False, partner_bank_id=False):
+            date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):
 
         data = super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id,
-            date_invoice, payment_term, partner_bank_id)
+            date_invoice, payment_term, partner_bank_id, company_id)
         if partner_id:
             part = self.pool.get('res.partner').browse(cr, uid, partner_id)
             data[data.keys()[0]]['wh_iva_rate'] =  part.wh_iva_rate
