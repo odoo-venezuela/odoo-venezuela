bbb42170e4046bbabe4fa8222d8237f2cc56c6fb
javier@vauxoo.com <>
2011-06-22 17:18:05 -0430

    [ADD] se agrega formato de cheque del banco banesco

---
+++ bank_management/bank/__openerp__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Humberto Arocha           <humberto@openerp.com.ve>
+#              Angelica Barrios          <angélicaisabelb@gmail.com>
+#              María Gabriela Quilarque  <gabrielaquilarque97@gmail.com>
+#              Javier Duran              <javier.duran@netquatro.com>             
+#    Planified by: Nhomar Hernande
+#    Finance by: Helados Gilda, C.A. http://heladosgilda.com.ve
+#    Audited by: Humberto Arocha humberto@openerp.com.ve
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    "name" : "Bank Management",
+    "version" : "0.1",
+    "author" : "Vauxoo",
+    "website" : "http://vauxoo.com",
+    "category": 'Generic Modules/Accounting',
+    "description": """
+    Proporciona los formatos para los cheques venezolanos
+    """,
+    'init_xml': [],
+    "depends" : [],
+    'update_xml': [
+        'bank/banesco_data.xml',        
+    ],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
