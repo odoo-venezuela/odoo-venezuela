7343e252039727507efc50be4fcee9e63354316b
Israel Fermín Montilla <ferminster@gmail.com>
2011-09-30 18:17:49 -0430

    [IMP] added the new wizard to the wizards' __init__.py file

--- l10n_ve_fiscal_requirements/wizard/__init__.py
+++ l10n_ve_fiscal_requirements/wizard/__init__.py
@@ -23,5 +23,5 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
-
+import wizard_nro_ctrl
 import wizard_invoice_nro_ctrl
