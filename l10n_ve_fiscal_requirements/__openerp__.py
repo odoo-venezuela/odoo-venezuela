76f21e265e26cb48b0aeaf1e32f8ee0dbe22f1c3
Nhomar Hernandez nhomar@vauxoo.com <>
2011-10-05 21:00:14 -0430

    [IMP] Informational data on module l10n_ve_fiscal_requirements

--- l10n_ve_fiscal_requirements/__openerp__.py
+++ l10n_ve_fiscal_requirements/__openerp__.py
@@ -28,35 +28,22 @@
     "category": 'Generic Modules/Accounting',
     "description": """Venezuelan Tax Laws Requirements:
     - Invoice Control Number.
-    - Tax-except concept
+    - Tax-except concept, necesary rule by Venezuelan Laws.
     - Required address invoice.
-    - Unique address invoice.
+    - Unique billing address (invoice), necesary rule by Venezuelan Laws.
     - VAT verification for Venezuela rules.
-    - Damaged "free forms" justification.
+    - Damaged "Legal free forms" declaration.
     - Tax Units configuration.
     ---------------------------------------------------------------------------
     For damaged invoices (Free form formats), you must go to the company and, under the configuration section,
     create the corresponding journal and account.
+    TODO : Include this on wizard configuration.
 
     If you install this module with invoice data on the database, the concept_id will be 
     Empty for all those invoices, so, when you try to modify them you have to add a value on
     that field
 
     This module should also install a menu item under the accounting configuration menu.
-    ------------------------TECH INFO-------------------------------------------
-    CHANGELOG:
-       - For the migration to the l10n_ve on OpenERP:
-         - Added the functionality to configure Tax Units, for this, it was necesary make this module depend on
-           account_accountant, to make visible de accounting configuration menu.
-         - Added the functionality to change the control number on an invoice (free form format)
-         - Changed the Invoice Ref label to Control Number
-         - Integrated the l10n_ve_nro_ctrl module functionality on this one
-         - Since the concept_id field is added by the l10n_ve_islr_withholding, an
-           error es rised if it's not installed and it is necesary to withhold
-         - If you need to withhold ISLR, you must install the module mentioned above
-         - Eliminated the dependencies with stock and purchase modules, migrated that functionalities to
-           other modules
-
     """,
     'init_xml': [
         'data/l10n_ut_data.xml',
