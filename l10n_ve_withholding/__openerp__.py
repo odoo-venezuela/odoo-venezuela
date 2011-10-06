302607e2c38a1bd1c4409cd71ff213c73f4e7343
Nhomar Hernandez nhomar@vauxoo.com <>
2011-10-05 21:02:14 -0430

    [IMP] Informational data on module l10n_ve_withholding

--- l10n_ve_withholding/__openerp__.py
+++ l10n_ve_withholding/__openerp__.py
@@ -27,9 +27,15 @@
     "author" : "Vauxoo",
     "website" : "http://vauxoo.com",
     "category": 'Generic Modules/Accounting',
-    "description": """Management withholdings:
-    - Add journal types
-    - Add 
+    "description": """- General Method for account move for Venezuela withholding process.
+- Add relation between account invoice tax and account tax, to avoid loss historical relation.
+- Add account journals types or withholding VAT and Income.
+- Make relation between account move and withholding documents with a method.
+- Add common menus for withholdin process.
+- Add common tabs on views where they will be used to add information for other modules related to Venezuela. localization.
+
+TODO:
+- 
     """,
     'init_xml': [],
     "depends" : ["l10n_ve_fiscal_requirements", "account", "account_accountant"],
