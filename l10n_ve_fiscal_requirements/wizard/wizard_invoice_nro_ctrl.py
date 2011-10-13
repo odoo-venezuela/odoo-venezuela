276bf43882635695f22ad1320ea3de890cabe18e
Israel Fermín Montilla <israel@openerp.com.ve>
2011-10-13 13:45:04 -0430

    [FIX] fixed the concept_id issue

--- l10n_ve_fiscal_requirements/wizard/wizard_invoice_nro_ctrl.py
+++ l10n_ve_fiscal_requirements/wizard/wizard_invoice_nro_ctrl.py
@@ -85,7 +85,7 @@ class wizard_invoice_nro_ctrl(osv.osv_memory):
                     })
                 break
         except:
-            del invoice_line['concept_id']
+            pass
         invoice_line_id = invoice_line_obj.create(cr, uid, invoice_line, {})
         return inv_id
 
