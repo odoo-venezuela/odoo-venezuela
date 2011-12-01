b3bfc4d705ed0c3225c2ef847aeed7fc8fcbbd76
Humberto Arocha <humbertoarocha@gmail.com>
2011-12-01 00:51:52 -0430

    [IMP] Improved socket method for creating moves in invoice so that other modules could use it in a easier way.
    [ADD] Using the previous socket method created, it was added
    a super method for creating the move to VAT Wihholding.
    
    Though the lines for taxes needs refactoring.

--- l10n_ve_withholding/invoice.py
+++ l10n_ve_withholding/invoice.py
@@ -31,11 +31,19 @@ from tools.translate import _
 
 class account_invoice(osv.osv):
     _inherit = 'account.invoice'
-
+    
+    
+    def _get_move_lines(self, cr, uid, ids, to_wh, period_id, 
+                            pay_journal_id, writeoff_acc_id, 
+                            writeoff_period_id, writeoff_journal_id, date, 
+                            name, context=None):
+        if context is None: context = {}
+        return []
+        
     def ret_and_reconcile(self, cr, uid, ids, pay_amount, pay_account_id, 
                             period_id, pay_journal_id, writeoff_acc_id, 
                             writeoff_period_id, writeoff_journal_id, date, 
-                            name, context=None):
+                            name, to_wh, context=None):
         if context is None:
             context = {}
         #TODO check if we can use different period for payment and the writeoff line
@@ -53,21 +61,42 @@ class account_invoice(osv.osv):
             'ref':invoice.number,
             'date': date,
             'currency_id': False,
+            'name':name
         }
-        l2 = {
-            'debit': direction * pay_amount<0 and - direction * pay_amount,
-            'credit': direction * pay_amount>0 and direction * pay_amount,
-            'account_id': pay_account_id,
-            'partner_id': invoice.partner_id.id,
-            'ref':invoice.number,
-            'date': date,
-            'currency_id': False,
-        }
+        lines = [(0, 0, l1)]
+        #########################################################
+        #  START TODO                                           #
+        #########################################################
+        l2 = self._get_move_lines(cr, uid, ids, to_wh, period_id, 
+                            pay_journal_id, writeoff_acc_id, 
+                            writeoff_period_id, writeoff_journal_id, date, 
+                            name, context=context)
+        if not l2:
+            raise osv.except_osv(_('Warning !'), _('No accounting moves were created.\n Please, Check if there are Taxes/Concepts to withhold in the Invoices!'))
+        print 'DESPUES DE LAS LINEAS 2' 
+        lines += l2
+        
+        print 'ACCOUNT\t\tDEBIT\t\tCREDIT'
+        for i in lines:
+            print '%s\t\t%s\t\t%s'%(i[2]['account_id'],i[2]['debit'],i[2]['credit'])
+        
+        #~ l2 = {
+            #~ 'debit': direction * pay_amount<0 and - direction * pay_amount,
+            #~ 'credit': direction * pay_amount>0 and direction * pay_amount,
+            #~ 'account_id': pay_account_id,
+            #~ 'partner_id': invoice.partner_id.id,
+            #~ 'ref':invoice.number,
+            #~ 'date': date,
+            #~ 'currency_id': False,
+        #~ }
+#~ 
+        #~ l2['name'] = name
 
-        l1['name'] = name
-        l2['name'] = name
-
-        lines = [(0, 0, l1), (0, 0, l2)]
+        #~ lines = [(0, 0, l1), (0, 0, l2)]
+        
+        #########################################################
+        #  END TODO                                             #
+        #########################################################
         move = {'ref': invoice.number, 'line_id': lines, 'journal_id': pay_journal_id, 'period_id': period_id, 'date': date}
         move_id = self.pool.get('account.move').create(cr, uid, move, context=context)
 
