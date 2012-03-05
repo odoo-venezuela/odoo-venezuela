dce82ea9fd5a6d66efeefec2627f0b2afea1b964
Humberto Arocha <humbertoarocha@gmail.com>
2012-03-05 10:18:38 -0430

    [FIX] Improve the way nro_ctrl and reference fields are taken into account

--- l10n_ve_fiscal_requirements/invoice.py
+++ l10n_ve_fiscal_requirements/invoice.py
@@ -36,7 +36,7 @@ class account_invoice(osv.osv):
             if inv.type in ('out_invoice','out_refund'):
                 return True
             inv_ids = self.search(cr,uid,
-                        ['|',('nro_ctrl','ilike',inv.nro_ctrl.strip()),('reference','ilike',inv.reference.strip()),
+                        ['|',('nro_ctrl','ilike',inv.nro_ctrl and inv.nro_ctrl.strip() ),('reference','ilike',inv.reference and inv.reference.strip()),
                         ('type','in',('in_invoice','in_refund')),
                         ('partner_id','=',inv.partner_id.id)],
                         context=context)
