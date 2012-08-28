a63f7d30fdd7b5456841414cbe181b5101c843ef
Tulio Ruiz <tulio@vauxoo.com>
2012-08-28 10:17:00 -0430

    [FIX] Added proper description

--- l10n_ve_fiscal_reports_V3/__openerp__.py
+++ l10n_ve_fiscal_reports_V3/__openerp__.py
@@ -37,6 +37,12 @@
     "description" : """
     What this module does:
     Build all Fiscal Reports for Law in Venezuela.
+    Add 2 new columns because of:
+    Según Articulo 77 del Reglamento de la Ley del IVA No.5.363 del 12 de Julio de 1999. 
+    Parágrafo Segundo: El registro de las operaciones contenidas en 
+    el reporte global diario generado por las máquinas fiscales, 
+    se reflejarán en el LIbro de Ventas del mismo modo que se establece 
+    respecto de los comprobantes que se emiten a no contibuyentes, indicando el número de registro de la máquina.
     -- Sales Report.
     """,
     "website" : "http://openerp.com.ve",
