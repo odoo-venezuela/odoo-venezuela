3a9e013aec3b95bb8d47faaa8c63a77a3a73bd13
Humberto Arocha <humbertoarocha@gmail.com>
2012-11-29 16:35:06 -0430

    [FIX] Missing import model on Fiscal Requirements

--- l10n_ve_fiscal_requirements/__init__.py
+++ l10n_ve_fiscal_requirements/__init__.py
@@ -22,5 +22,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
+import model 
 import wizard
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
