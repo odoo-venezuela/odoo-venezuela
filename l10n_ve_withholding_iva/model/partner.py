8d2877c4298cc18c666d4236e3d70669536a67da
Humberto Arocha <humbertoarocha@gmail.com>
2012-12-10 14:12:10 +0000

    [FIX] Fixing signatures for _parser_dom

--- l10n_ve_withholding_iva/model/partner.py
+++ l10n_ve_withholding_iva/model/partner.py
@@ -52,12 +52,12 @@ class seniat_url(osv.osv):
 
     _inherit = 'seniat.url'
     
-    def _parse_dom(self,dom,rif,url_seniat,context=None):
+    def _parse_dom(self,cr,uid,dom,rif,url_seniat,context=None):
         su_obj = self.pool.get('seniat.url')
         wh_agent = dom.childNodes[0].childNodes[1].firstChild.data.upper()=='SI' and True or False
-        wh_rate = su_obj._buscar_porcentaje(rif,url_seniat)
+        wh_rate = su_obj._buscar_porcentaje(cr, uid, rif)
         self.logger.info("RIF: %s Found" % rif)
         data = {'wh_iva_agent':wh_agent,'wh_iva_rate':wh_rate}
-        return dict(data.items() + super(seniat_url,self)._parse_dom(dom,rif,url_seniat,context=context).items())
+        return dict(data.items() + super(seniat_url,self)._parse_dom(cr,uid,dom,rif,url_seniat,context=context).items())
     
 seniat_url()
