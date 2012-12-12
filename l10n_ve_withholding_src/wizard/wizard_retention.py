1499c40f3b5fb256c5d5c667891ca4f609268bea
Yanina Aular <yanina.aular@vauxoo.com>
2012-12-12 12:40:24 -0430

    [ADD] Files to wizard change retention

---
+++ l10n_ve_withholding_src/wizard/wizard_retention.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Vauxoo C.A.           
+#    Planified by: Humberto Arocha
+#    Audited by: Vauxoo C.A.
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+import wizard
+from osv import osv
+from osv import fields
+from tools.translate import _
+
+class wiz_retention(osv.osv_memory):
+    _name = 'wiz.retention'
+    _description = "Wizard that changes the retention value"
+
+    def set_retention(self, cr, uid, ids, context=None):
+        if context is None:
+            context={}
+        data = self.pool.get('wiz.retention').read(cr, uid, ids)[0]
+        if not data['sure']:
+            raise osv.except_osv(_("Error!"), _("Please confirm that you want to do this by checking the option"))
+        
+        inv_obj = self.pool.get('account.invoice')
+        n_retention = data['name']
+                
+        if n_retention and n_retention > 5:
+            raise osv.except_osv(_("Error!"), _("Maximum retention is 5%"))
+        else:
+            invoice = inv_obj.browse(cr, uid, context['active_id'])
+            inv_obj.write(cr, uid, context.get('active_id'), {'wh_src_rate': n_retention}, context=context)
+        
+        return {}
+
+    _columns = {
+        'name': fields.float('Retention Value', required=True),
+        'sure': fields.boolean('Are you sure?'),
+    }
+    
+wiz_retention()
+
