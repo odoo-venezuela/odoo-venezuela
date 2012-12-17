76077a530432224ebe72447e5be6728843a55f36
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2012-12-17 12:02:17 -0430

    [FIX] Adaptation for compatibility with OpenERP 7.0 wizard. Erased the line import wizard. ---- ES_es [FIX] Adaptación para compatibilidad de los wizard con Openerp 7.0. Se borro la linea import wizard.

--- l10n_ve_fiscal_requirements/wizard/update_info_partner.py
+++ l10n_ve_fiscal_requirements/wizard/update_info_partner.py
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import wizard
 import pooler
 from osv import osv, fields
 from tools.translate import _
