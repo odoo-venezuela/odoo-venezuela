03b434ad8514064769980d25c5bb4b1e92cced34
Humberto Arocha <humbertoarocha@gmail.com>
2012-12-21 13:35:00 -0430

    [FIX] Missing context on method signature

--- l10n_ve_withholding_islr/model/invoice.py
+++ l10n_ve_withholding_islr/model/invoice.py
@@ -163,7 +163,7 @@ class account_invoice(osv.osv):
                 {'invoice_id':inv_id,'islr_wh_doc_id':islr_wh_doc_id}))
         return iwhdi_ids
 
-    def _create_islr_wh_doc(self,cr,uid,ids):
+    def _create_islr_wh_doc(self,cr,uid,ids,context=None):
         '''
         Funcion para crear en el modelo islr_wh_doc
         '''
