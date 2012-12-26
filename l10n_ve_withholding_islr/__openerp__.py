0909a426ac4e7e6dcf9358e25faba32b6971b281
Humberto Arocha <humbertoarocha@gmail.com>
2012-12-26 13:30:58 -0430

    [FIX] Making corrections to some misspellings

--- l10n_ve_withholding_islr/__openerp__.py
+++ l10n_ve_withholding_islr/__openerp__.py
@@ -32,11 +32,11 @@
     "author" : "Vauxoo",
     "category" : "General",
     "website": "http://wiki.openerp.org.ve/",
-    "description": ''' - Generate the voucher of withholding income to validate the invoice.
- - Generate the report of voucher of withholding income.
- - Generate the file .xml required by the Venezuelan laws, for agent of withholding income specials.
+    "description": ''' - Generate the voucher of income withholding to validate the invoice.
+ - Generate the report of voucher of income withholding.
+ - Generate the file .xml required by the Venezuelan laws, for agent of income withholding specials.
  - Generate view for load the concepts of wittholding whith their rates.
- - Generate view for visualize the withholding income for suppilers and customers.
+ - Generate view for visualize the income withholding for suppilers and customers.
  - Load data of the 86 concepts of wittholdings whith their rates.
  - Send an email to supplier with approved withholdings, if not opt_out in the partner
 If you want be able to propose withholding concepts from sales and purchases you must install extra module @section{l10n_ve_sale_purchase}.
