4930cd8b60e4d46c842926c102b7b32e27e10698
Humberto Arocha <humbertoarocha@gmail.com>
2013-01-21 06:29:23 -0430

    [IMP] Including the rate_id in the concept being withheld and the code

--- l10n_ve_withholding_islr/model/invoice.py
+++ l10n_ve_withholding_islr/model/invoice.py
@@ -259,7 +259,7 @@ class account_invoice(osv.osv):
                 'ref':inv_brw.number,
                 'date': date,
                 'currency_id': False,
-                'name':name
+                'name':_('%s - Concept: %s')%(name,iwdl_brw.islr_rates_id.code)
             }))
         
         return res
