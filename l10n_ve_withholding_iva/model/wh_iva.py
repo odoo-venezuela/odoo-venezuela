63630ddf1a3967bbf6b63a1968e6cf565f88b8fc
Humberto Arocha <humbertoarocha@gmail.com>
2013-03-13 11:28:58 -0430

    [REG]  Reverting regression

--- l10n_ve_withholding_iva/model/wh_iva.py
+++ l10n_ve_withholding_iva/model/wh_iva.py
@@ -148,8 +148,8 @@ class account_wh_iva_line(osv.osv):
         'base_ret': fields.function(_amount_all, method=True, digits=(16,4), string='Wh. amount', multi='all', help="Withholding without tax amount"),
         'move_id': fields.many2one('account.move', 'Account Entry', readonly=True, help="Account entry"),
         'wh_iva_rate': fields.float(string='Withholding Vat Rate', digits_compute= dp.get_precision('Withhold'), help="Withholding vat rate"),
-        'date': fields.related('retention_id', 'date', type='date', relation='account.wh.iva', string='Date', store='True'),
-        'date_ret': fields.related('retention_id', 'date_ret', type='date', relation='account.wh.iva', string='Date Withholding', store='True')
+        'date': fields.related('retention_id', 'date', type='date', relation='account.wh.iva', string='Date'),
+        'date_ret': fields.related('retention_id', 'date_ret', type='date', relation='account.wh.iva', string='Date Withholding')
     }
 
     _sql_constraints = [
