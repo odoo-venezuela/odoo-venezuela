3f24bd02c3c1dce4984705aea83fce91e4826840
joyza <joyza@vauxoo.com>
2013-03-13 16:37:18 -0430

    [ADD]Performed the VAT Write Off module, and designed the view and model

---
+++ l10n_ve_vat_write_off/__openerp__.py
@@ -0,0 +1,16 @@
+{    
+    "name" : "VAT Write Off",
+    "version" : "0.5",
+    "depends" : ["l10n_ve_fiscal_book"],
+    "author" : "Vauxoo",
+    "description" : """
+    """,
+    "website" : "http://openerp.com.ve",
+    "category" : "Generic Modules/Accounting",
+    "data" : [
+        "view/l10n_ve_vat_write_off.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
+
---
+++ l10n_ve_vat_write_off/model/l10n_ve_vat_write_off.py
@@ -0,0 +1,25 @@
+from openerp.osv import fields, osv
+
+
+class vat_write_off(osv.osv):
+    _description = ''
+    _name = 'vat.write.off'
+    _columns = {
+        'company_id':fields.many2one('res.company','Company',
+            help='Company',required=True),
+        'period_id':fields.many2one('account.period','Period',
+            help="Book's Fiscal Period",required=True),
+        'state': fields.selection([('draft','Getting Ready'),
+            ('open','Approved by Manager'),('done','Seniat Submitted')],
+            string='Status', required=True),          
+        'purchase_fb_id':fields.many2one('fiscal.book', 'Purchase Fiscal Book',
+            help='Purchase Fiscal Book'), 
+        'sale_fb_id':fields.many2one('fiscal.book', 'Sale Fiscal Book',
+            help='Sale Fiscal Book'), 
+        
+    }
+    _defaults = {
+        'state': 'draft',
+        'company_id': lambda s,c,u,ctx: \
+            s.pool.get('res.users').browse(c,u,u,context=ctx).company_id.id,
+    }
