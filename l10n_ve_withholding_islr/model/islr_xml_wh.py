88479d6a3ed2e6089e469c38c3e0d366e4f1e0ec
Yanina Aular <yanina.aular@vauxoo.com>
2013-04-12 16:04:44 -0430

    [FIX] to the import module

--- l10n_ve_withholding_islr/model/islr_xml_wh.py
+++ l10n_ve_withholding_islr/model/islr_xml_wh.py
@@ -25,7 +25,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
-slr_xml_wh.pyfrom openerp.osv import osv
+from openerp.osv import osv
 from openerp.osv import fields
 from openerp.tools.translate import _
 from openerp.tools import config
