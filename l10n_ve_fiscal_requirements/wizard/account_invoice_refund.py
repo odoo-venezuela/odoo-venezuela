dff4a3c6ff64e8d29ee12d7120aa556aa860b361
Yanina Aular <yanina.aular@vauxoo.com>
2013-04-26 16:32:25 -0430

    [FIX] comments and symbols

--- l10n_ve_fiscal_requirements/wizard/account_invoice_refund.py
+++ l10n_ve_fiscal_requirements/wizard/account_invoice_refund.py
@@ -128,7 +128,7 @@ class account_invoice_refund(osv.osv_memory):
         """ 
         @param ids: the account invoice refund’s ID or list of IDs
         """
-        wzd_brw = self.browse(cr,uid,ids[0],context=context)_
+        wzd_brw = self.browse(cr,uid,ids[0],context=context)
         brw = self.browse(cr,uid,ids[0],context=context)
         inv_obj = self.pool.get('account.invoice')
         reconcile_obj = self.pool.get('account.move.reconcile')
