93f30003041b04075c69f57e00e24cfad5dcc4a7
Katherine Zaoral (Vauxoo) <katherine.zaoral@vauxoo.com>
2013-04-30 15:25:43 -0430

    [IMP] more data at fiscal book module description

--- l10n_ve_fiscal_book/__openerp__.py
+++ l10n_ve_fiscal_book/__openerp__.py
@@ -35,16 +35,17 @@
     "depends" : ["account","l10n_ve_withholding_iva","l10n_ve_fiscal_requirements","l10n_ve_imex"],
     "author" : "Vauxoo",
     "description" : """
-    What this module does:
-    Build all Fiscal Reports for Law in Venezuela.
-    Add 2 new columns because of:
-    Según Articulo 77 del Reglamento de la Ley del IVA No.5.363 del 12 de Julio de 1999. 
-    Parágrafo Segundo: El registro de las operaciones contenidas en 
-    el reporte global diario generado por las máquinas fiscales, 
-    se reflejarán en el LIbro de Ventas del mismo modo que se establece 
-    respecto de los comprobantes que se emiten a no contibuyentes, indicando el número de registro de la máquina.
-    -- Sales Report.
-    """,
+Fiscal Book
+===============================================================================
+Build all Fiscal Reports for Law in Venezuela.
+Add 2 new columns because of:
+
+Según Articulo 77 del Reglamento de la Ley del IVA No.5.363 del 12 de Julio de 1999. 
+Parágrafo Segundo: El registro de las operaciones contenidas en el reporte global diario generado por las máquinas fiscales, se reflejarán en el Libro de Ventas del mismo modo que se establece respecto de los comprobantes que se emiten a no contibuyentes, indicando el número de registro de la máquina.
+
+.. note::
+    * *El libro de ventas no contempla la sección de Ventas por cuenta de Terceros.*
+""",
     "website" : "http://openerp.com.ve",
     "category" : "Generic Modules/Accounting",
     "init_xml" : [
