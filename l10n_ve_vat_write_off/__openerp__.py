b60da972378ed2c7d5cf0272f701f1a3d3b0b632
Humberto Arocha <humbertoarocha@gmail.com>
2013-05-02 12:31:41 -0430

    [FIX] Fixing inverted xml order in __openerp__.py descriptor file getting rid of unneeded import on __init__.py descriptor file

--- l10n_ve_vat_write_off/__openerp__.py
+++ l10n_ve_vat_write_off/__openerp__.py
@@ -9,8 +9,8 @@
     "website" : "http://openerp.com.ve",
     "category" : "Generic Modules/Accounting",
     "update_xml" : [
-        "view/l10n_ve_vat_write_off.xml",
         "report/l10n_ve_vat_write_off_report.xml",
+        "view/l10n_ve_vat_write_off.xml",
     ],
     "active": False,
     "installable": True,
