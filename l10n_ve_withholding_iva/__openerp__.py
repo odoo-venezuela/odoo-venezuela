033c446670ecafe5b8fd61eabcd681e82a5d8ceb
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2013-12-09 11:07:26 -0430

    [ADD][l10n_ve_withholding_iva] new test yaml for txt iva documents in purchase operations. Case 7, 7.1 and 7.2, Only done withholding documents can be add to a txt document.

--- l10n_ve_withholding_iva/__openerp__.py
+++ l10n_ve_withholding_iva/__openerp__.py
@@ -87,12 +87,13 @@
 #            'test/iva_test_wh_4.yml',
 
 
-             'test/purchase_invoice_wh_iva.yml',
-             'test/sale_invoice_wh_iva.yml',
-             'test/sale_wh_iva.yml',
-             'test/purchase_wh_iva.yml',
-             'test/purchase_wh_iva_sequence.yml',
-             'test/sale_wh_iva_sequence.yml'
+#             'test/purchase_invoice_wh_iva.yml',
+#             'test/sale_invoice_wh_iva.yml',
+#             'test/sale_wh_iva.yml',
+#             'test/purchase_wh_iva.yml',
+#             'test/purchase_wh_iva_sequence.yml',
+#             'test/sale_wh_iva_sequence.yml',
+             'test/purchase_wh_iva_txt.yml',
         ],
     'installable': True,
     'active': False,
