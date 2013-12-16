35bd32fff8c93787b58c1cf10212cc840271846c
Humberto Arocha <humbertoarocha@gmail.com>
2013-12-16 13:49:20 -0430

    [FIX][ovl] Changes in the description of the OVL

--- ovl/__openerp__.py
+++ ovl/__openerp__.py
@@ -20,7 +20,7 @@
 #
 ##############################################################################
 {   "name" : "OpenERP Venezuelan Localization",
-    "version" : "3.0",
+    "version" : "4.0",
     "depends" : [
                  # Level Zero of Modules 
                  "document",
@@ -51,23 +51,28 @@ Install all apps needed to comply with Venezuelan laws
 
 This module will install for you:
 
- - "l10n_ve_imex",
+  -  l10n_ve_fiscal_requirements 
 
- - "l10n_ve_fiscal_requirements", 
+  -  l10n_ve_split_invoice
 
- - "l10n_ve_split_invoice", 
+  -  l10n_ve_generic
 
- - "l10n_ve_withholding", 
+  -  l10n_ve_imex
 
- - "l10n_ve_withholding_iva" ,
+  -  l10n_ve_withholding 
 
- - "l10n_ve_fiscal_reports_V3",
+  -  l10n_ve_withholding_iva
 
- - "l10n_ve_withholding_muni",
+  -  l10n_ve_withholding_islr
 
- - "l10n_ve_withholding_src",
+  -  l10n_ve_withholding_muni
 
- - "l10n_ve_withholding_muni",
+  -  l10n_ve_withholding_src
+
+  -  l10n_ve_fiscal_book
+
+  -  l10n_ve_vat_write_off
+
 
 Optionals (Not installed by default), uncomment on your/addons/path/ovl/__openerp__.py file this dependencies if you want to use them
 
