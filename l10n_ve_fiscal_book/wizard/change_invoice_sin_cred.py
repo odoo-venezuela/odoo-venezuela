71cd427bdd5f538ef18b8ae54f0c18106f3862ef
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-09-19 10:27:59 -0430

    [ADD] new wizard basic model structure.

---
+++ l10n_ve_fiscal_book/wizard/change_invoice_sin_cred.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Katherine Zaoral <kathy@vauxoo.com>
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+import decimal_precision as dp
+
+
+class change_invoice_sin_credwizard(osv.TransientModel):
+
+    """
+    Wizard that changes the invoice sin cred attribute.
+    """
+    _name = 'change.invoice.sin.cred'
+    _description = 'Change Invoice Sin Cred'
+    _columns = {
+        'sin_cred': fields.boolena('Sin Cred', required=True),
+    }
