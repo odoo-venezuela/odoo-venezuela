fa5779658f59745e9ac9ebbc96411e3ee743287e
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-12-08 13:25:19 -0430

    [pylint] Fix C0103 - Invalid variable name "r". Change to inv_brw

--- l10n_ve_fiscal_book/invoice.py
+++ l10n_ve_fiscal_book/invoice.py
@@ -68,9 +68,9 @@ class inherited_invoice(osv.osv):
         for i in ids:
             ret.update({i: ''})
         if res:
-            for r in res:
-                if not r.get_is_imported:
-                    ret.update({r.id: r.date_invoice})
+            for inv_brw in res:
+                if not inv_brw.get_is_imported:
+                    ret.update({inv_brw.id: inv_brw.date_invoice})
 #                ret =r.date_document
         return ret
 
